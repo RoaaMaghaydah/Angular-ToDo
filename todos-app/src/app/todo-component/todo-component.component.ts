e2740d935240e87aab3986c2198d0eed1f926da9
RoaaMaghaydah <roaa.maghaydah@gmail.com>
2021-08-25 00:01:35 +0300

    todo list app

---
+++ todos-app/src/app/todo-component/todo-component.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-component',
+  templateUrl: './todo-component.component.html',
+  styleUrls: ['./todo-component.component.css']
+})
+export class TodoComponentComponent implements OnInit {
+  @Input() newTodos:string[]= []
+  list:string="";
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+add(data:string):void{
+  console.log("add")
+  this.newTodos.push(data)
+}
+}
